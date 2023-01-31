303b06ac92ef936e25cf7d392d34e5e800250d86
dennisnderitu254 <dknderitu@gmail.com>
2023-01-31 15:37:06 +0300

    Ruby script that accepts one argument and pass it to a regular expression matching method

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,3 +1,4 @@
 #!/usr/bin/env ruby
 # Prints all the words that start with h, end with n, and have between 2 and 5 t's in between.
+# ./1-repetition_token_0.rb  School | cat -e
 puts ARGV[0].scan(/hbt{2,5}n/).join
