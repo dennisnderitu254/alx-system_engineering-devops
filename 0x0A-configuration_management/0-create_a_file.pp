bfc4ca3c7c181b4531d517c51943c9a76f003e44
dennisnderitu254 <dknderitu@gmail.com>
2023-03-24 12:52:24 +0300

    removing duplicate filepath in script

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,8 @@
 # Creates a file
 file { '/tmp/school':
   ensure  => file,
-  path    => '/tmp/school',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  content => 'I love Puppet'
+  content => 'I love Puppet',
 }
